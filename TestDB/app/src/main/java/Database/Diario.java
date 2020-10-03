fc4b7bdeb37edc69a9da1f655b8f9495b439dace
MartiSosto <44461808+MartiSosto@users.noreply.github.com>
2020-10-03 23:42:13 +0200

    Test DB 4

---
+++ TestDB/app/src/main/java/Database/Diario.java
@@ -0,0 +1,11 @@
+package Database;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Report.class}, version = 1, exportSchema = false)
+public abstract class Diario extends RoomDatabase {
+    public abstract ReportDao reportDao();
+
+}
--- TestDB/app/src/main/java/Database/NewReportActivity.java
+++ TestDB/app/src/main/java/Database/NewReportActivity.java
@@ -1,24 +1,16 @@
 package Database;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.testdb_3.R;
 
+import android.os.Bundle;
+
+import com.example.testdb_4.R;
 
 public class NewReportActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_new_report);
-
-
     }
 }
